7f612cfd39b9335d7be322a47452160a8ef1f6b4
fullstack82 <fullstack8210@gmail.com>
2020-12-15 22:55:26 +0100

    changes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,36 +11,42 @@ function Cards() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
-                            src='images/img-9.jpg'
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='adventure'
-                            path='/experiences'
+                            src='img/29.jpg'
+                            text='Ride through the Sahara Desert on a guided camel tour'
+                            label='Adrenaline'
+                            
                         />
                         <CardItem
-                            src='images/img10.jpg'
+                            src='img/2.jpg'
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='adventure'
+                            
+                        />
+                        <CardItem
+                            src='img/10.jpg'
                             text='Travel through the Islands of Bali in a Private Cruise'
                             label='luxury'
-                            path='/experiences'
+                            
                         />
                     </ul>
                     <ul className="cards__items">
                         <CardItem
-                            src='images/img-home.jpg'
+                            src='img/12.jpg'
                             text='Set Sail in the Atlantic Ocean visiting Ucharted Waters'
                             label='Relax'
-                            path='/experiences'
+                            
                         />
                         <CardItem
-                            src='images/img-7.jpg'
+                            src='img/7.jpg'
                             text='The wildest experiences'
                             label='Adventure'
-                            path='/experiences'
+                            
                         />
                         <CardItem
-                            src='images/img-8.jpg'
+                            src='img/8.jpg'
                             text='Ride through the Sahara Desert on a guided camel tour'
                             label='Adrenaline'
-                            path='/sing-up'
+                            
                         />
                     </ul>
                 </div>
